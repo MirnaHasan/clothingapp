d11074a48450ab337ad90d0d64f8152c96aba329
Mirna Hasan <mirnamhasan50@gmail.com>
2024-10-29 14:39:58 +0330

    first commit

---
+++ clothes_app/lib/screens/homepagescreen.dart
@@ -0,0 +1,18 @@
+
+
+
+
+import 'package:clothes_app/widgets/homepagebody.dart';
+import 'package:flutter/material.dart';
+
+class Homepagescreen extends StatelessWidget {
+  const Homepagescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body:Homepagebody(),
+    );
+  }
+}
---
+++ clothes_app/lib/widgets/homepagebody.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class Homepagebody extends StatefulWidget {
+  const Homepagebody({super.key});
+
+  @override
+  State<Homepagebody> createState() => _HomepagebodyState();
+}
+
+class _HomepagebodyState extends State<Homepagebody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
